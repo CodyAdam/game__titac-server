60e9da2a0030c2671a41e1429c19960962db2a93
CodyAdam <cody.ad4@gmail.com>
2021-07-09 15:38:44 +0200

    Server online

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
 import { createServer } from "http";
+import { ServerOptions } from 'https';
 import { Server, Socket } from "socket.io";
 
 const httpServer = createServer();
-const io = new Server(httpServer, {
-  // ...
-});
+const io = new Server(httpServer, { cors: { origin: "*" } });
+
+function logCount() {
+  console.log(`Sockets connected : ${io.sockets.sockets.size}`);
+
+}
 
 io.on("connection", (socket: Socket) => {
-  // ...
+  console.log("  +  New connection");
+  logCount()
+
+  socket.on('state', (state) => {
+    console.log(`Received from client : ${state}`);
+    socket.broadcast.emit('state', state);
+  })
+
+  socket.on('ping', (msg) => socket.broadcast.emit('ping', msg))
+
+  socket.on('disconnect', () => {
+    console.log('  -  One disconnected');
+    logCount()
+  })
 });
 
-httpServer.listen(3000);
+
+
+httpServer.listen(8080);
