0f85025be0f4b8de03104d75e84b1a5bdc3f2955
CodyAdam <cody.ad4@gmail.com>
2021-07-09 20:35:59 +0200

    Update on connection

--- build/server.js
+++ build/server.js
@@ -4,21 +4,39 @@ var http_1 = require("http");
 var socket_io_1 = require("socket.io");
 var httpServer = http_1.createServer();
 var io = new socket_io_1.Server(httpServer, { cors: { origin: "*" } });
+var lastState = {
+    received: true,
+    score: { p1: [], p2: [] },
+    turn: Math.random() < 0.5 ? 1 : 2,
+    winner: false,
+    grid: [
+        { slotsIndex: 0, value: [] },
+        { slotsIndex: 0, value: [] },
+        { slotsIndex: 0, value: [] },
+        { slotsIndex: 0, value: [] },
+        { slotsIndex: 0, value: [] },
+        { slotsIndex: 0, value: [] },
+        { slotsIndex: 0, value: [] },
+        { slotsIndex: 0, value: [] },
+        { slotsIndex: 0, value: [] },
+    ]
+};
 io.on("connection", function (socket) {
     console.log("  +  New connection");
     logCount();
+    socket.emit('state', lastState);
     socket.on('state', function (state) {
         console.log("Received from client : " + state);
+        lastState = state;
         socket.broadcast.emit('state', state);
     });
-    socket.on('ping', function (msg) { return socket.broadcast.emit('ping', msg); });
     socket.on('disconnect', function () {
-        console.log('  -  One disconnected');
+        console.log('  -  Disconnected');
         logCount();
     });
 });
 console.log('Server started');
 function logCount() {
-    console.log("Sockets connected : " + io.sockets.sockets.size);
+    console.log("     Sockets connected : " + io.sockets.sockets.size);
 }
 httpServer.listen(2222);
--- src/server.ts
+++ src/server.ts
@@ -3,30 +3,65 @@ import { Server, Socket } from "socket.io";
 import fs from 'fs'
 import path from 'path'
 
+export type Direction = 'tr' | 'mr' | 'br' | 'lc' | 'mc' | 'rc' | 'brtl' | 'bltr';
+
+export interface BoardState {
+  received: boolean,
+  score: { p1: Array<Direction>, p2: Array<Direction> }
+  turn: 1 | 2
+  winner: false | 1 | 2
+  grid: Array<Tile>
+}
+
+export interface Tile {
+  slotsIndex: number,
+  value: Array<Item>
+}
+
+export interface Item { used: boolean, player: 1 | 2 }
+
 const httpServer = createServer();
 const io = new Server(httpServer, { cors: { origin: "*" } });
 
+let lastState: BoardState = {
+  received: true,
+  score: { p1: [], p2: [] },
+  turn: Math.random() < 0.5 ? 1 : 2,
+  winner: false,
+  grid: [
+    { slotsIndex: 0, value: [] },
+    { slotsIndex: 0, value: [] },
+    { slotsIndex: 0, value: [] },
+    { slotsIndex: 0, value: [] },
+    { slotsIndex: 0, value: [] },
+    { slotsIndex: 0, value: [] },
+    { slotsIndex: 0, value: [] },
+    { slotsIndex: 0, value: [] },
+    { slotsIndex: 0, value: [] },
+  ]
+}
 
 io.on("connection", (socket: Socket) => {
   console.log("  +  New connection");
   logCount()
 
-  socket.on('state', (state) => {
+  socket.emit('state', lastState);
+
+  socket.on('state', (state: BoardState) => {
     console.log(`Received from client : ${state}`);
+    lastState = state;
     socket.broadcast.emit('state', state);
   })
 
-  socket.on('ping', (msg) => socket.broadcast.emit('ping', msg))
-
   socket.on('disconnect', () => {
-    console.log('  -  One disconnected');
+    console.log('  -  Disconnected');
     logCount()
   })
 });
 
 console.log('Server started');
 function logCount() {
-  console.log(`Sockets connected : ${io.sockets.sockets.size}`);
+  console.log(`     Sockets connected : ${io.sockets.sockets.size}`);
 }
 
 
